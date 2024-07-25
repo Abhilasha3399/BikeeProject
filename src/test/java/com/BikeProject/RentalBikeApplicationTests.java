6ddfbc5747b9d44ddf8b23b866ba28ef842f2017
Abhilasha <pabhilashapatel09@gmail.com>
2024-07-25 12:01:12 +0530

    first commit

---
+++ src/test/java/com/BikeProject/RentalBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
